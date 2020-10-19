d8ec09692e6a839613ea7f5f83d5e5098630a0de
heuzin <matheusqs.arq@gmail.com>
2020-10-19 11:15:12 -0400

    implementing react router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WorkoutApp from './components/WorkoutApp';
+import AppRouter from '../src/routers/AppRouter'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 let appRoot = document.getElementById('app');
 
-ReactDOM.render(<WorkoutApp />, appRoot);
+ReactDOM.render(<AppRouter />, appRoot);
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header'
+import { BrowserRouter } from 'react-router-dom';
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <Header />
+    </BrowserRouter>
+)
+
+export default AppRouter;
