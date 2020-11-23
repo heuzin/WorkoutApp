314f8a43f28137534b4c956eb93e69b52b9d3cb1
heuzin <matheusqs.arq@gmail.com>
2020-11-23 12:36:27 -0500

    setup firebase

---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AITH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGIN_SENDER_ID,
+    appId: process.env.FIREBASE__APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database()
+
+export { firebase, database as default}
